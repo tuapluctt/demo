e2009dcb5a305047727b8084f8d33bbde05e6ec6
User Name <hoangvantunkv@gmail.com>
2023-08-25 20:20:02 +0700

    delete acc2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world11111!");
+        System.out.println("Hello world111diahdaidhaidhai11!");
     }
 }
